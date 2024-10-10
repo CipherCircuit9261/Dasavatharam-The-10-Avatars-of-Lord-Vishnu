fc83e57c6299697083bdfb9e634cafa67bf9325f
SoftwareEngineer2010 <aprameyananantha@gmail.com>
2024-10-10 18:30:59 -0400

    Add files via upload

---
+++ Dasavatharam.js
@@ -0,0 +1,33 @@
+ console.log("Welcome to Dasavatharam");
+ console.log("This is the Dasavatharam Website");
+ console.log("The 10 Avatars of Lord Vishnu");
+ console.log("I am the Developer,Creator, and Designer of this Website ")
+ console.log("My Name is Aprameyan Krishnan Anantha")
+ console.log("I am interested in Web Development and Robotics")
+ console.log("I am a Web Developer and Robotics Enthusiast")
+ console.log(" The website you are currently(Dasavatharam) is my first official website I made.")
+ console.log("Below is the Fibionacci Sequence just for fun")
+
+ /* Ps:: If you are a smart cookie and decide to look in the source page then heres some more facts about me
+ I am 14 yearsold(At the time I am Making this website)
+ My Birthday is April 26 2010
+ and I Love Coding, Programming, Science, and Robotics
+ Hope you love my work
+ */
+ 
+ let fn1 =0
+let fn2 =1
+nextFibonacci = fn1 +fn2
+console.log("Fibonacci sequence up to 100 terms")
+
+for(let i=1;i<=200;i++){
+    console.log(nextFibonacci)
+    fn1 = fn2;
+    fn2 = nextFibonacci;
+    nextFibonacci = fn1 + fn2;
+    console.log(nextFibonacci);
+}
+
+
+ 
+
